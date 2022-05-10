b2082ff4e2deb17f614c07db34fb118ab4850ace
bkfan1 <blossomsc15@gmail.com>
2022-05-10 11:28:08 -0400

    Added Loader component to the return state of MyApp component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Loader from "../components/misc/Loader";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Loader/>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
