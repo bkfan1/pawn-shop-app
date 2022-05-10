408bd345b6a77a60e97eb10cc61b1136cebc9ab1
bkfan1 <blossomsc15@gmail.com>
2022-05-10 11:27:32 -0400

    Created loader component

---
+++ components/misc/Loader.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from "react";
+import { useRouter } from "next/router";
+
+export default function Loader(){
+
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const handleStart = (url) => url !== router.asPath && setLoading(true);
+  
+      const handleComplete = (url) =>
+        url !== router.asPath && setTimeout(() => setLoading(false), 1200);
+  
+      router.events.on("routeChangeStart", handleStart);
+      router.events.on("routeChangeComplete", handleComplete);
+      router.events.on("routeChangeError", handleComplete);
+  
+      return () => {
+        router.events.off("routeChangeStart", handleStart);
+        router.events.off("routeChangeComplete", handleComplete);
+        router.events.off("routeChangeError", handleComplete);
+      };
+    });
+  
+    return loading && <div className="spinner-wrapper">
+  
+      <figure className="lds-dual-ring"></figure>
+  
+    </div>
+
+}
