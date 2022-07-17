bcfd250d007fd2e6ece25052299e2b433d2d9513
bkfan1 <blossomsc15@gmail.com>
2022-07-17 14:24:16 -0400

    Modified 404 error message

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,11 @@
 import Layout from "../components/layout/Layout";
 
-export default function Error() {
+export default function NotFoundPage() {
   return (
     <Layout>
       <figure className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
         <i className="bi bi-exclamation-triangle-fill has-text-white is-size-3" />
-        <h1 className="has-text-white is-size-4">Ha ocurrido un error.</h1>
+        <h1 className="has-text-white is-size-4">El recurso no existe o no ha sido encontrado.</h1>
       </figure>
     </Layout>
   );
