d2f9a9171e8bae9d111927aced27d8f41f224550
bkfan1 <blossomsc15@gmail.com>
2022-07-17 15:36:46 -0400

    Removed unused imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Loader from "../components/misc/Loader";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
